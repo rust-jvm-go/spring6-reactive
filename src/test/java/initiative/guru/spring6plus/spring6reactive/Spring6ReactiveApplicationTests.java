8a8726f7b5740486c41f2f1a055951fbb13fd0ef
rustxjvmxgo <rustxjvmxgo@gmail.com>
2023-03-19 21:30:16 +0800

    Initial commit

---
+++ src/test/java/initiative/guru/spring6plus/spring6reactive/Spring6ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package initiative.guru.spring6plus.spring6reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
