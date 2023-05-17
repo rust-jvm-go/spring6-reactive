8d38dc0fcb5e3b1c34525f7a53b348228298f900
rustxjvmxgo <rustxjvmxgo@gmail.com>
2023-05-17 14:01:08 +0800

    Road to Flyway / YugabyteDB

--- src/main/java/initiative/guru/spring6plus/spring6reactive/config/DatabaseConfig.java
+++ src/main/java/initiative/guru/spring6plus/spring6reactive/config/DatabaseConfig.java
@@ -1,28 +1,46 @@
 package initiative.guru.spring6plus.spring6reactive.config;
 
+import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
+import jakarta.annotation.Nonnull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+import org.springframework.r2dbc.connection.R2dbcTransactionManager;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+import org.springframework.transaction.ReactiveTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableR2dbcAuditing
-public class DatabaseConfig {
+@EnableTransactionManagement
+public class DatabaseConfig extends AbstractR2dbcConfiguration {
 
     @Value("classpath:/schema.sql")
     Resource resource;
 
     @Bean
-    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
 
         ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
         initializer.setConnectionFactory(connectionFactory);
         initializer.setDatabasePopulator(new ResourceDatabasePopulator(resource));
 
         return initializer;
+    }
+
+    @Override
+    @Nonnull
+    public ConnectionFactory connectionFactory() {
+        return ConnectionFactories.get("r2dbc:pool:h2:mem:///spring6reactive?options=DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+    }
+
+    @Bean
+    public ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory) {
+        return new R2dbcTransactionManager(connectionFactory);
     }
 }
